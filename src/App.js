382f372f52ee967f69b944cd59778fb2f090d987
Jonah Simpson <jonahs99@gmail.com>
2019-08-29 15:26:34 -0400

    Create db entry

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { newGrid } from './db.js'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	useEffect(() => {
+		newGrid('first')		
+	})
+
+	return (
+		<div className="App">
+		<header className="App-header">
+		<img src={logo} className="App-logo" alt="logo" />
+		<p>
+		Edit <code>src/App.js</code> and save to reload.
+		</p>
+		<a
+		className="App-link"
+		href="https://reactjs.org"
+		target="_blank"
+		rel="noopener noreferrer"
+		>
+		Learn React
+		</a>
+		</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/db.js
@@ -0,0 +1,47 @@
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDuihQslEyfAK216hIjgwVbsNdXO4oga-g",
+	authDomain: "great-grid.firebaseapp.com",
+	databaseURL: "https://great-grid.firebaseio.com",
+	projectId: "great-grid",
+	storageBucket: "great-grid.appspot.com",
+	messagingSenderId: "46709538232",
+	appId: "1:46709538232:web:074bede9c9289bf5"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+
+const defaultTiles = [
+	{ color: '#000', chr: 'A' },
+	{ color: '#111', chr: 'B' },
+	{ color: '#222', chr: 'C' },
+	{ color: '#333', chr: 'D' },
+	{ color: '#444', chr: 'E' },
+	{ color: '#555', chr: 'F' },
+	{ color: '#666', chr: 'G' },
+	{ color: '#777', chr: 'H' },
+	{ color: '#888', chr: 'I' },
+	{ color: '#999', chr: 'J' },
+];
+
+const eight = new Array(8).fill(0)
+const defaultGrid = Object.fromEntries(
+	eight.flatMap((_, i) => eight.map((_, j) => [`${i},${j}`, 0]))
+)
+
+console.log(defaultGrid)
+
+export async function newGrid(id) {
+	const doc = await db.collection('grids').add({
+		size: { rows: 8, cols: 8 },
+		tiles: defaultTiles,
+		grid: defaultGrid,
+	})
+
+	return doc
+}
+

806e542b07a348e1d96af6d89c7e883daaa544e0
knar <zakjsimpson@gmail.com>
2019-08-30 15:47:25 -0400

    Add setSize database function

--- src/db.js
+++ src/db.js
@@ -77,3 +77,10 @@ export async function setGrid(id, row, col, tile) {
 		[`grid.${row},${col}`]: tile,
 	})
 }
+
+export async function setSize(id, { rows, cols }) {
+	await db.collection('grids').doc(id).update({
+		'grid.size.rows': rows,
+		'grid.size.cols': cols,
+	})
+}
